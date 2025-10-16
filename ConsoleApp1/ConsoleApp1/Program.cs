54c5fc16658cfbf0eeb719ae3fc4d1a3f3c3ae1a
Aleix <aleix.maynou.7e9@itb.cat>
2025-10-16 12:40:37 +0200

    We create a program that calculates the area of a rectangle

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.ComponentModel;
-namespace CelsiusToKelvin
+namespace RectangleArea
 {
     public class Program
     {
         public static void Main()
         {
-            //The user is prompted to input a temperature in Celsius.
-            const string Input = "Enter temperature in Celsius:";
-            const string Result = "Temperature in Kelvin:";
-            double celsius,kelvin;
-            Console.WriteLine(Input);
-            celsius = Convert.ToInt32(Console.ReadLine());
-            kelvin = (celsius + 273);
-            Console.WriteLine(Result, kelvin);
-            // 'kelvin' contains the converted temperature, and it is displayed.
+            //The user is prompted to input the width and height of a rectangle.
+            const string Input_Width = "Enter the width of the rectangle:";
+            const string Input_Height = "Enter the height of the rectangle:";
+            const string Result = "The area of the rectangle is:";
+            double width,height,area;
+            Console.WriteLine(Input_Width);
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Input_Height);
+            height = Convert.ToInt32(Console.ReadLine());
+            area = (width*height);
+            Console.WriteLine(Result, area);
+            // 'area' contains the product of 'width' and 'height', and it is displayed.
+
 
 
         }
