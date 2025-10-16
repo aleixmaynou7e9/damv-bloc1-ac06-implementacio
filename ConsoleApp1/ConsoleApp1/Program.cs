6ae0f521da6abccc38214aa6669a41cdccabb8be
Aleix <aleix.maynou.7e9@itb.cat>
2025-10-16 11:45:21 +0200

    We create a program that sums two integers.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,19 @@ namespace HelloWorldProject
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //The user is prompted to input two integers.
+            const string Input1 = "Enter the first integer:";
+            const string Input2 = "Enter the second integer:";
+            const string Result = "The sum is:";
+            int num1,num2,sum;
+            Console.WriteLine(Input1);
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Input2);
+            num2=Convert.ToInt32(Console.ReadLine());
+            sum = (num1 + num2);
+            Console.WriteLine(sum);
+            //'sum' contains the sum of 'num1' and 'num2', and it is displayed.
+
         }
     }
 }
